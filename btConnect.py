fcee6d8427d74d1487205b5e91fc398d826e4ba3
fire1ce <stas@3os.org>
2021-07-06 10:12:06 +0300

    refinement

--- btConnect.py
+++ btConnect.py
@@ -3,48 +3,36 @@ import time
 import sched
 import subprocess
 atStation = '192.168.100.75'
-en5Command = "ifconfig en13 | grep 'inet ' | awk '{print $2}'"
+ifconfigCommand = "ifconfig en13 | grep 'inet ' | awk '{print $2}'"
 edifierStatusCommand = "blueutil --is-connected '04-fe-a1-a2-fe-f8'"
 edifierConnectCommand = "blueutil --connect '04-fe-a1-a2-fe-f8'"
 edifierDisconnectCommand = "blueutil --disconnect '04-fe-a1-a2-fe-f8'"
-lastIP = ''
 
 s = sched.scheduler(time.time, time.sleep)
 
 
 def timerInterval(sc):
-    global lastIP
 
     getCurrentIP = subprocess.run(
-        [en5Command], shell=True, text=True, capture_output=True)
+        [ifconfigCommand], shell=True, text=True, capture_output=True)
     currentIP = getCurrentIP.stdout.strip()
 
     checkConnectedBT = subprocess.run(
         [edifierStatusCommand], shell=True, text=True, capture_output=True)
     edifierStatus = checkConnectedBT.stdout.strip()
 
-    if currentIP != lastIP and currentIP == atStation:
-        print("At Station - Connect")
-        print("lastIP is: ", lastIP)
-        print("currentIP is: ", currentIP)
+    if currentIP == atStation and edifierStatus == '0':
+        print("At station, Connecting")
+        print("Current IP is: ", currentIP)
         subprocess.run(
             [edifierConnectCommand], shell=True)
 
-    if currentIP == atStation and currentIP == lastIP and edifierStatus == '0':
-        print("Same IP Not Connected")
-        print("lastIP is: ", lastIP)
-        print("currentIP is: ", currentIP)
-        subprocess.run(
-            [edifierConnectCommand], shell=True)
-
-    if currentIP != lastIP and currentIP != atStation:
-        print("left Station - Dissconed")
-        print("lastIP is: ", lastIP)
-        print("currentIP is: ", currentIP)
+    if currentIP != atStation and edifierStatus == '1':
+        print("left station, Disconecting")
+        print("Current IP is: ", currentIP)
         subprocess.run(
             [edifierDisconnectCommand], shell=True)
-
-    lastIP = currentIP
+    print("Loop")
 
     s.enter(20, 1, timerInterval, (sc,))
 
